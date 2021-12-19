d294e7c5f2410a90d7f128594fab5507b32b5967
Cameron McCurdy <cam.alex.mccurdy@gmail.com>
2021-12-19 03:26:16 -0500

    Time: 84 ms (71.03%), Space: 40.9 MB (31.58%) - LeetHub

---
+++ two-sum/two-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var storage = {};
+    for (var i = 0; i < nums.length; i++) {
+        if (storage[target - nums[i]] !== undefined) {
+            return [storage[target - nums[i]], i];
+        } else {
+            storage[nums[i]] = i;
+        }
+    }
+};
