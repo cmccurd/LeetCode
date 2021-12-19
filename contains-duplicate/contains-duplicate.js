ec723377d62b3e5998a3d5c693623046754ae1b9
Cameron McCurdy <cam.alex.mccurdy@gmail.com>
2021-12-19 03:50:21 -0500

    Time: 96 ms (42.81%), Space: 47.9 MB (9.15%) - LeetHub

--- contains-duplicate/contains-duplicate.js
+++ contains-duplicate/contains-duplicate.js
@@ -3,15 +3,6 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    var storage = {
-        [nums[0]]: 1,
-    };
-    for (var i = 1; i < nums.length; i++) {
-        if (storage[nums[i]]) {
-            return true;
-        } else {
-            storage[nums[i]] = 1;
-        }
-    }
-    return false;
+    const set = new Set(nums);
+    return set.size !== nums.length;
 };
