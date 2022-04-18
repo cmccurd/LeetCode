ce471b440f22291a33c0c7d1e4a777016c37772a
Cameron McCurdy <cam.alex.mccurdy@gmail.com>
2022-04-18 01:46:15 -0400

    Time: 117 ms (68.40%), Space: 49.7 MB (18.65%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    //hold longest substring value
+    //use sliding window algo to find all substrings
+    //create a map of current chars
+    //return the value
+    
+    let longest = 0;
+    let i = 0;
+    var map = {};
+    for (var j = 0; j < s.length; j++) {
+        if (map[s[j]] === undefined || map[s[j]] < i) {
+            map[s[j]] = j;
+            longest < s.substring(i, j + 1).length ? longest = s.substring(i, j + 1).length : null;
+        } else {
+            i = map[s[j]] + 1;
+            map[s[j]] = j;
+        }
+    }
+    return longest;
+};
+
+//given
+//s = "abcdaf" bcdaf
+//s = "abcdbfg" cdbfg
+
+//s = "tmmzuxt" mzuxt
