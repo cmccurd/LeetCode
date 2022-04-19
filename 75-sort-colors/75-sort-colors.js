689a0b179babb1502586705bc40b8e0d8bfe55c0
Cameron McCurdy <cam.alex.mccurdy@gmail.com>
2022-04-19 02:25:21 -0400

    Time: 56 ms (96.35%), Space: 42.1 MB (70.84%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    //use two pointers technique
+    //right pointer will be the inner edge of 1/2
+    let right = nums.length - 1;
+    let left = 0;
+    let current = left;
+    
+    var swap = (a, b) => {
+        [nums[a], nums[b]] = [nums[b], nums[a]];
+    }
+    while (current <= right) {
+        if (nums[current] === 2) {
+            swap(current, right);
+            right--;
+        } else if (nums[current] === 0) {
+            swap(current, left);
+            left++;
+            current++;
+        } else {
+            current++;
+        }
+    }
+    
+    return nums;
+};
