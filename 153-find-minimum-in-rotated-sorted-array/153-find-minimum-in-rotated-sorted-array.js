e7df6b90b23cccd2409a1628c11c0b95f876c948
Cameron McCurdy <cam.alex.mccurdy@gmail.com>
2022-04-29 17:03:58 -0400

    Time: 67 ms (72.49%), Space: 42.3 MB (29.56%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
@@ -3,24 +3,24 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    //min is val on left that is greater than value on right.
-    //max is val on left is greather than right
     var left = 0;
     var right = nums.length - 1;
+    
+    var start = nums[left];
     var end = nums[right];
     
     while (left <= right) {
         var mid = left + Math.floor((right - left)/2);
         var val = nums[mid];
         var valL = nums[mid - 1];
-        var valR = nums[mid + 1];
-        if (valL > valR) {
-            return val < valR ? val : valR;
-        } else if (val > end) {
-            left = mid + 1;
-        } else {
+        
+        if (val < valL) {
+            return val;
+        } else if (val < start) {
             right = mid - 1;
+        } else {
+            left = mid + 1;
         }
     }
-    return nums[left]
+    return start;
 };
