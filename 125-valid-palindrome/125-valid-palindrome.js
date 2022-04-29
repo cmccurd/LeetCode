72bd776fdeac804aa2469df8c7f28a3d3de22f78
Cameron McCurdy <cam.alex.mccurdy@gmail.com>
2022-04-28 23:04:47 -0400

    Time: 108 ms (38.72%), Space: 43.6 MB (97.81%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -6,7 +6,6 @@ var isPalindrome = function(s) {
     s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
     var l = 0;
     var r = s.length - 1;
-    console.log(s);
     while (l < r) {
         if (s[l] !== s[r]) {
             return false;
