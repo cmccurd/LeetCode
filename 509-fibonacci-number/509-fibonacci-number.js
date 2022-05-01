fa1c760dbba5fdb897bad568279907302b061f93
Cameron McCurdy <cam.alex.mccurdy@gmail.com>
2022-04-30 20:52:23 -0400

    Time: 119 ms (21.31%), Space: 42.2 MB (26.58%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -2,12 +2,13 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n) {
+var fib = function(n, memo = {}) {
     if (n === 0) {
         return 0;
     }
-    if (n === 1) {
+    if (n <= 2) {
         return 1;
     }
-    return fib(n-1) + fib(n-2);
+    memo[n] = fib(n-1, memo) + fib(n-2, memo)
+    return memo[n];
 };
