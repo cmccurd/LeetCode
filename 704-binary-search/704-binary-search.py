9d4bb41265744ccfeb633b2d81f3bda3b6d4d35d
Cameron McCurdy <cam.alex.mccurdy@gmail.com>
2022-07-11 03:48:55 -0400

    Time: 401 ms (35.66%), Space: 15.5 MB (22.26%) - LeetHub

---
+++ 704-binary-search/704-binary-search.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        
+        
+        pos = int(r / 2)
+        if nums[pos] == target: 
+            return pos
+        while l <= r:
+            if nums[pos] > target:
+                r = pos - 1
+                pos = int(l + (r - l) / 2)
+            elif nums[pos] < target:
+                l = pos + 1
+                pos = int(l + (r - l) / 2)
+            else:
+                return pos
+        return -1
