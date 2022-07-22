f3b2a80b3635465574dba5f36016b1bacf8025ac
Cameron McCurdy <cam.alex.mccurdy@gmail.com>
2022-07-22 08:18:56 -0400

    Time: 259 ms (20.07%), Space: 53.3 MB (58.93%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -4,17 +4,11 @@
  */
 var groupAnagrams = function(strs) {
     
-    const map = {}
-    for (var i = 0; i < strs.length; i++) {
-        var count = new Array(26).fill(0)
-        for (j = 0; j < strs[i].length; j++) {
-            count[strs[i].charCodeAt(j) - 'a'.charCodeAt(0)]++
-        }
-        if (map[count]) {
-            map[count].push(strs[i])
-        } else {
-            map[count] = [strs[i]]
-        }
+    const map = {};
+    for (const str of strs) {
+        const key = [...str].sort();
+        
+        map[key] ? map[key].push(str) : map[key] = [str]
     }
-    return Object.values(map)
+    return Object.values(map);
 };
