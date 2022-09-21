59edee34fc5e3746027b768d00a214b9a82b41d9
Cameron McCurdy <cam.alex.mccurdy@gmail.com>
2022-09-21 09:36:19 -0400

    Time: 142 ms (36.17%), Space: 42.5 MB (94.17%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,5 +4,20 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    return s.split('').sort().join() === t.split('').sort().join();
+    if (s.length !== t.length) {
+        return false;
+    }
+    
+    var sArr = new Array(26).fill(0);
+    var tArr = new Array(26).fill(0);
+    
+    for (var i = 0; i < s.length; i++) {
+        var sChar = s[i];
+        var tChar = t[i];
+        var a = "a";
+        
+        sArr[sChar.charCodeAt(0) - a.charCodeAt(0)]++;
+        tArr[tChar.charCodeAt(0) - a.charCodeAt(0)]++;
+    }
+    return sArr.join() === tArr.join();
 };
